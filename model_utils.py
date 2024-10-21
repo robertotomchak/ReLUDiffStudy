d531d2c304fdb61672f4eb64d490ed9153489f3d
robertotomchak <robertotomchak@gmail.com>
2024-10-21 18:38:13 -0300

    Add mobile net functions

--- model_utils.py
+++ model_utils.py
@@ -3,6 +3,10 @@ defining some useful functions for creating, modifying and training the desired 
 '''
 
 import torch
+from torch import nn
+import torchvision.models as models
+import torchvision
+
 import relu
 
 def replace_layers(model, old, new):
@@ -21,6 +25,27 @@ def reset_model(module):
         if hasattr(layer, "reset_parameters"):
             layer.reset_parameters()
         reset_model(layer)
+
+
+def change_layer(model, layer_name, new_layer):
+    model._modules[layer_name] = new_layer
+
+
+def create_mobile_net(activation_hardwish, activation_relu, last_output, pre_trained=False):
+    if pre_trained:
+        model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
+    else:
+        model = models.mobilenet_v3_small()
+    replace_layers(model, [nn.Hardswish], activation_hardwish)
+    replace_layers(model, [nn.ReLU], activation_relu)
+    out_layer = nn.Sequential(
+        nn.Linear(in_features=576, out_features=1024, bias=True),
+        activation_relu,
+        nn.Dropout(p=0.2, inplace=True),
+        nn.Linear(in_features=1024, out_features=last_output, bias=True)
+    )
+    change_layer(model, "classifier", out_layer)
+    return model
 
 
 def train(dataloader, model, loss_fn, optimizer, device):
