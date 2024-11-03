c4eae10480b00117ed3a3e2863ac391934c9fd3a
RobertoTomchak <robertotomchak@gmail.com>
2024-11-03 10:48:37 -0300

    Fix bugs and refactor

--- test_activation_speed.py
+++ test_activation_speed.py
@@ -2,27 +2,58 @@ import torch
 import torch.nn as nn
 from time import time
 
-N = 10_000_000
+N = 1_000_000
 EXECUTIONS = 100
 
-def test_activation(activation1, activation2, device, N=1_000_000, executions=10):
-    total1, total2 = 0, 0
-    for _ in range(executions):
-        values = torch.rand(N, device=device)
-        time1 = time()
-        ans1 = activation1(values)
-        time1 = time() - time1
+def test_activation(activation, device, n=1_000_000, executions=10):
+    total = 0
+    values = torch.randn(size=(executions, n), device=device, requires_grad=True)
+    # warmup
+    activation(values)
 
-        time2 = time()
-        ans2 = activation2(values)
-        time2 = time() - time2
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    t = time()
+    for i in range(executions):
+        ans = activation(values[i])
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    t = time() - t
+    return t / executions
 
-        total1 += time1
-        total2 += time2
-    return total1 / executions, total2 / executions
+def test_activation_diff(activation, device, n=1_000_000, executions=10):
+    total = 0
+    values = torch.randn(size=(executions, n), device=device, requires_grad=True)
+    # warmup
+    ans = activation(values)
+    ans.backward(torch.ones_like(values))
+
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    t = time()
+    for i in range(executions):
+        values[i].grad = None
+        ans = activation(values[i])
+        ans.backward(torch.ones_like(values[i]))
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    t = time() - t
+    return t / n
+
+def compare_activations(activation1, activation2, name1, name2, device):
+    activation1.to(device)
+    activation2.to(device)
+    time_activation1 = test_activation(activation1, device, n=N, executions=EXECUTIONS)
+    time_activation2 = test_activation(activation2, device, n=N, executions=EXECUTIONS)
+    time_activation1_b = test_activation_diff(activation1, device, n=N, executions=EXECUTIONS)
+    time_activation2_b = test_activation_diff(activation2, device, n=N, executions=EXECUTIONS)
+    print(f"{name1} vs {name2}")
+    print(f"FORWARD: {name1} is {round(time_activation1 / time_activation2, 2)}x slower than {name2}")
+    print(f"BACKWARD: {name1} is {round(time_activation1_b / time_activation2_b, 2)}x slower than {name2}")
+    print("-"*30)
 
 device = (
-    "cuda"
+    "cuda:1"
     if torch.cuda.is_available()
     else "mps"
     if torch.backends.mps.is_available()
@@ -35,30 +66,6 @@ print(f"Executions = {EXECUTIONS}")
 print(f"Device = {device}")
 print(f"-"*30)
 
-
-relu = nn.ReLU()
-gelu = nn.GELU()
-relu.to(device)
-gelu.to(device)
-
-time_relu, time_gelu = test_activation(relu, gelu, device, N=N, executions=EXECUTIONS)
-print("ReLU vs GELU")
-print(f"ReLU: {time_relu} seconds")
-print(f"GELU: {time_gelu} seconds")
-diff = (time_gelu - time_relu) / time_relu
-print(f"GELU is {round(100*diff, 2)}% slower than ReLU")
-print("-"*30)
-
-
-hardswish = nn.Hardswish()
-swish = nn.SiLU()
-hardswish.to(device)
-swish.to(device)
-
-time_hs, time_s = test_activation(relu, gelu, device, N=N, executions=EXECUTIONS)
-print("Hardswish vs Swish")
-print(f"Hardswish: {time_hs} seconds")
-print(f"Swish: {time_s} seconds")
-diff = (time_s - time_hs) / time_hs
-print(f"Swish is {round(100*diff, 2)}% slower than Harswish")
-print("-"*30)
+compare_activations(nn.GELU(), nn.ReLU(), "GELU", "ReLU", device)
+compare_activations(nn.SiLU(), nn.Hardswish(), "Swish", "Hardswish", device)
+compare_activations(nn.Sigmoid(), nn.Hardsigmoid(), "Sigmoid", "HardSigmoid", device)
